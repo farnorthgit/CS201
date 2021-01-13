21eed0066fd361b6e2a09cb5432db33b4afd4934
farnorthgit <markcbillingsley@gmail.com>
2021-01-12 20:43:23 -0900

    Initial Commit

---
+++ Hello world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello world
+//
+//  Created by Mark Billingsley on 1/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
