e662004af38a2f16afc59d0d91c9c57eada43beb
farnorthgit <markcbillingsley@gmail.com>
2021-01-15 21:25:41 -0900

    inserted final comments

--- HW0/HW0 - hello world/HW0 - hello world/main.cpp
+++ HW0/HW0 - hello world/HW0 - hello world/main.cpp
@@ -3,14 +3,12 @@
 //  HW0 - hello world
 //
 //  Created by Mark Billingsley on 1/15/21.
-//  Trying a stupid number of times to make git match my local disk.
-//  Finding my errors, but also it seems finicky.
-//  x
 //
 
 #include <iostream>
 
 int main() {
+//  uses for loop to print hello world five times rather than using cout five times
     for (int i = 0; i < 5; i++) {
         std::cout << "Hello, World!\n";
     }
