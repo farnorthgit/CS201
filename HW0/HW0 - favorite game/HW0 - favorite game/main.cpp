1c5a39888aa5521a74ac7beafbd3271b1a387f35
farnorthgit <markcbillingsley@gmail.com>
2021-01-15 20:07:05 -0900

    again

--- HW0/HW0 - favorite game/HW0 - favorite game/main.cpp
+++ HW0/HW0 - favorite game/HW0 - favorite game/main.cpp
@@ -1,27 +1,13 @@
 //
 //  main.cpp
-//  Hello world
+//  HW0 - favorite game
 //
-//  Created by Mark Billingsley on 1/12/21.
+//  Created by Mark Billingsley on 1/15/21.
 //
 
 #include <iostream>
 
 int main() {
-    // insert code here...
-    for (int i = 0; i < 5; i++) {
-        std::cout << "Hello, World!\n";
-    }
-        
-    std::cout << "Aw, princess on the steeple and all the pretty people\n";
-    std::cout << "They're all drinkin', thinkin' that they got it made\n";
-    std::cout << "Exchangin' all precious gifts\n";
-    std::cout << "But you'd better take your diamond ring, ya better pawn it, babe\n";
-    std::cout << "You used to be so amused\n";
-    std::cout << "At Napoleon in rags and the language that he used\n";
-    std::cout << "Go to him now, he calls ya, ya can't refuse\n";
-    std::cout << "When ya ain't got nothin', you got nothin' to lose\n";
-    std::cout << "You're invisible now, ya got no secrets to conceal\n";
     std::cout << "UNO is the classic card game that’s easy to pick up and impossible to put down! Players take turns matching a card in their hand with the current card shown on top of the deck either by color or number. Special action cards deliver game-changing moments as they help you defeat your opponents. These include Skips, Reverses, Draw Twos, Wild and Draw Four Wild cards. You’ll find 25 of each color (red, green, blue, and yellow), eight Wild cards, three Customizable cards and one Special Rule card inside the 112-card deck. If you can’t make a match, you must draw from the central pile! And when you’re down to one card, don’t forget to shout “UNO!” The first player to rid themselves of all the cards in their hand before their opponents wins. It’s Fast Fun for Everyone!\n";
     return 0;
 }
