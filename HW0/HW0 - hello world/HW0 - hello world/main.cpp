5911e8f8f3fca9fc4f17cbd5c65a118a53d13a08
farnorthgit <markcbillingsley@gmail.com>
2021-01-15 20:19:13 -0900

    still messing with this

---
+++ HW0/HW0 - hello world/HW0 - hello world/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HW0 - hello world
+//
+//  Created by Mark Billingsley on 1/15/21.
+//  Trying a stupid number of times to make git match my local disk.
+//  Finding my errors, but also it seems finicky.
+//  x
+//
+
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Hello, World!\n";
+    }
+}
