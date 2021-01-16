68db0f3c6a3009bc280579f4333efb07336f62d0
farnorthgit <markcbillingsley@gmail.com>
2021-01-15 19:14:29 -0900

    Created 3 different projects in Xcode (one for each program).  Restructured file system (again).

---
+++ HW0/HW0 - poem/HW0 - poem/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  HW0 - poem
+//
+//  Created by Mark Billingsley on 1/15/21.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Aw, princess on the steeple and all the pretty people\n";
+    std::cout << "They're all drinkin', thinkin' that they got it made\n";
+    std::cout << "Exchangin' all precious gifts\n";
+    std::cout << "But you'd better take your diamond ring, ya better pawn it, babe\n";
+    std::cout << "You used to be so amused\n";
+    std::cout << "At Napoleon in rags and the language that he used\n";
+    std::cout << "Go to him now, he calls ya, ya can't refuse\n";
+    std::cout << "When ya ain't got nothin', you got nothin' to lose\n";
+    std::cout << "You're invisible now, ya got no secrets to conceal\n";
+    return 0;
+}
