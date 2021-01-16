a46d507fca24e35ba1b5b201843cfa7b378192ef
farnorthgit <markcbillingsley@gmail.com>
2021-01-15 21:25:26 -0900

    inserted final comments

--- HW0/HW0 - favorite game/HW0 - favorite game/main.cpp
+++ HW0/HW0 - favorite game/HW0 - favorite game/main.cpp
@@ -3,12 +3,12 @@
 //  HW0 - favorite game
 //
 //  Created by Mark Billingsley on 1/15/21.
-//  y
 //
 
 #include <iostream>
 
 int main() {
+//  used cout once and pasted a full description of the card game UNO
     std::cout << "UNO is the classic card game that’s easy to pick up and impossible to put down! Players take turns matching a card in their hand with the current card shown on top of the deck either by color or number. Special action cards deliver game-changing moments as they help you defeat your opponents. These include Skips, Reverses, Draw Twos, Wild and Draw Four Wild cards. You’ll find 25 of each color (red, green, blue, and yellow), eight Wild cards, three Customizable cards and one Special Rule card inside the 112-card deck. If you can’t make a match, you must draw from the central pile! And when you’re down to one card, don’t forget to shout “UNO!” The first player to rid themselves of all the cards in their hand before their opponents wins. It’s Fast Fun for Everyone!\n";
     return 0;
 }
