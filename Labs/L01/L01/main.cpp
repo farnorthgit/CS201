aab39ee78f78a8ce6fc479a41270f2d1c34a7aa3
farnorthgit <markcbillingsley@gmail.com>
2021-01-22 22:10:43 -0900

    diamond program - seems to be done except need to deal with input of positive decimal

---
+++ Labs/L01/L01/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  L01
+//
+//  Created by Mark Billingsley on 1/22/21.
+//
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Labs/L02/L02/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  L02
+//
+//  Created by Mark Billingsley on 1/22/21.
+//  This program takes input of integer and prints the square of the next higher integer.
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    int month = 0;
+    int day = 0;
+    int year = 0;
+    cout << "Please enter an integer: ";
+    cin >> month >> day >> year;
+    cout << month << "/" << day << "/" << year << endl;
+    return 0;
+}
