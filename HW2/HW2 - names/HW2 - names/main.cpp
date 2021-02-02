0b6f8d8e288229bb4e7ed12e09cc92d21b5c398b
farnorthgit <markcbillingsley@gmail.com>
2021-02-01 22:31:33 -0900

    added 2nd function to search for a name

--- HW2/HW2 - names/HW2 - names/main.cpp
+++ HW2/HW2 - names/HW2 - names/main.cpp
@@ -1,7 +1,5 @@
-//
 //  main.cpp
 //  HW2 - names
-//
 //  Created by Mark Billingsley on 1/30/21.
 //  input names via a function, search the names, prints the names, and performs one other function on the list of names
 
@@ -13,6 +11,7 @@ using std::vector;
 using std::string;
 using std::cout;
 using std::cin;
+using std::endl;
 
 // using the program provided in the homework and making it a function
 void Inputnames(vector<string>&names) {
@@ -24,9 +23,26 @@ void Inputnames(vector<string>&names) {
     }
 }
 
-// testing whether the Inputnames function works
+// creating a function that returns a bool value to indicate whether a name exists in the list as determined by a search of the list;
+bool DoesNameExist(const string &nameToFind, const vector<string> &names) {
+    for (int j=0; j<names.size(); j++) {
+        if (nameToFind == names.at(j)) {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+
+// testing whether the Inputnames and DoesNameExist functions works
 int main() {
     vector<string> names;
     Inputnames(names);
+    string nameToFind;
+    cout << "Please enter name to find: ";
+    cin >> nameToFind;
+    if (DoesNameExist(nameToFind, names) == true)
+        cout << "Yes this name is on the list." << endl;
+    else cout << "No this name is not on the list." << endl;
     return 0;
 }
