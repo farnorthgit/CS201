f552f5914547240e169724fb1bdcdcac59cb2725
farnorthgit <markcbillingsley@gmail.com>
2021-02-10 22:34:30 -0900

    HW3 - boxer - created additional functions in cpp file to encapsulate linewithspace and ribbon

--- HW3/HW - boxer/HW - boxer/buildbox.cpp
+++ HW3/HW - boxer/HW - boxer/buildbox.cpp
@@ -10,11 +10,40 @@
 using std::cout;
 using std::endl;
 
-void buildbox(std::string statement, int boxsize) {
-    for (int i=0; i<boxsize; i++) {
-        for (int j=0; j<(statement.size() + 2 + (2*boxsize)); j++)
+// function called by buildbox function below
+// print the top rows full of asterisks
+void ribbon (std::string word, int size) {
+    for (int i=0; i<size; i++) {
+        for (int j=0; j<(word.size() + 2 + (2*size)); j++)
             cout << "*";
         cout << endl;
     }
-    
+}
+
+// function called by buildbox function below
+// print line above message that has asterisks on sides but spaces in middle
+void linewithspaces (std::string word, int size) {
+    for (int k=0; k<size; k++)
+        cout << "*";
+    for (int m=0; m<(word.size() + 2); m++)
+        cout << " ";
+    for (int k=0; k<size; k++)
+        cout << "*";
+    cout << endl;
+}
+
+// function called by main program
+void buildbox(std::string statement, int boxsize) {
+    ribbon(statement, boxsize);
+    linewithspaces(statement, boxsize);
+#if 0
+// print the line with the message
+    for (int k=0; k<boxsize; k++)
+        cout << "*";
+    for (int m=0; m<(statement.size() + 2); m++)
+        cout << " ";
+    for (int k=0; k<boxsize; k++)
+        cout << "*";
+    cout << endl;
+#endif
 }
