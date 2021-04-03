629b1949246d1691b6aa3ea55e2f55c2fcac8522
farnorthgit <markcbillingsley@gmail.com>
2021-04-02 22:15:35 -0800

    HW6 random map: RandomBetweenU function is working

--- HW6/HW6 random-map/HW6 random-map/main.cpp
+++ HW6/HW6 random-map/HW6 random-map/main.cpp
@@ -9,11 +9,17 @@
 #include <random>
 #include <cmath>
 
-//  int RandomBetweenU(intfirst,intlast) returns a uniform random number between first and last, inclusively (e.g. 1 and 6 returns numbers between 1 and 6).  Use the <random> header and std::map<int,int> to simulate a histogram
-//int RandomBetweenU(int first,int last) {
-//}
+// Seed with a real random value, if available
+std::random_device r;
+std::default_random_engine e1(r());
 
-//  int RandomBetweenN(intfirst,intlast) which returns a normally distributed random number between first and last, inclusively (e.g. 1 and 6 returns numbers between 1 and 6).  Use the <random> header and std::map<int,int> to simulate a histogram
+//  int RandomBetweenU(intfirst,intlast) returns a uniform random number between first and last, inclusively (e.g. 1 and 6 returns numbers between 1 and 6).
+int RandomBetweenU(int first,int last) {
+    std::uniform_int_distribution<int> uniform_dist(first, last);
+    return std::round(uniform_dist(e1));
+}
+
+//  int RandomBetweenN(intfirst,intlast) which returns a normally distributed random number between first and last, inclusively (e.g. 1 and 6 returns numbers between 1 and 6).
 //int RandomBetweenN(int first,int last) {
 //}
 
@@ -30,31 +36,27 @@ void PrintDistribution(const std::map<int,int>&numbers) {
 }
 
 int main() {
-    // Seed with a real random value, if available
-    std::random_device r;
- 
-    // Choose a random mean between 1 and 6
-    std::default_random_engine e1(r());
-    std::uniform_int_distribution<int> uniform_dist(1, 6);
-    int mean = uniform_dist(e1);
+    // Generate a mean and a normal distribution around that mean
+    std::uniform_int_distribution<int> uniform_dist_for_mean(1, 6);
+    int mean = uniform_dist_for_mean(e1);
     std::cout << "Randomly-chosen mean: " << mean << '\n';
- 
-    // Generate a normal distribution around that mean
     std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
     std::mt19937 e2(seed2);
     std::normal_distribution<> normal_dist(mean, 2);
  
+// Generate a normal distribution.  Use the <random> header and std::map<int,int> to simulate a histogram
     std::map<int, int> histnormal;
     for (int n = 0; n < 10000; ++n) {
         ++histnormal[std::round(normal_dist(e2))];
     }
-    
-//  Generate a uniform distribution between 1 and 6, and print its histogram
+
+// Generate a uniform distribution.  Use the <random> header and std::map<int,int> to simulate a histogram
     std::map<int, int> histuniform;
     for (int n = 0; n < 10000; n++) {
-        ++histuniform[std::round(uniform_dist(e1))];
+        ++histuniform[RandomBetweenU(1, 12)];
     }
 
+    
 //  Compare all three random number generators and print their histogram
     std::cout << "Normal distribution around " << mean << ":\n";
     PrintDistribution(histnormal);
